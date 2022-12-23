2dc63434c9347caf8da8e1b20b84d336432b336e
gloria <gloria.gao115@gmail.com>
2022-12-23 13:16:24 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const mainEl = document.querySelector('main')
+mainEl.remove()
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory")
+
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "is the champion"
